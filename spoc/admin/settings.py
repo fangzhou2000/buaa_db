f41b3947b1c00ec30656868e5cca3b44ecd8d1af
Mango <2732834341@qq.com>
2021-09-29 18:31:24 +0800

    Date base setting

--- spoc/admin/settings.py
+++ spoc/admin/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +85,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spoc',
+        'USER': 'root',
+        'PASSWORD': 'myja&*$4X579cKr',
+        'HOST': 'rm-2zeu3f7e1n5yt10v0co.mysql.rds.aliyuncs.com',
+        'PORT': '3306',
     }
 }
 
