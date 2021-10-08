42e5ed43e75036b8b857c682852739507ccef431
Mango <2732834341@qq.com>
2021-10-09 02:55:37 +0800

    🌈 style(backend_models): delete futile models

--- spoc/backend/models.py
+++ spoc/backend/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 import pymysql
-
-# 这些可以删除，但不要执行migrate
-class Student(models.Model):
-    username = models.CharField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=32)
-
-class teacher(models.Model):
-    username = models.CharField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=32)
--- spoc/backend/views.py
+++ spoc/backend/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
 from .base_mysql import *
 
 
