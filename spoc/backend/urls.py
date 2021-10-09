4988ee33b5fdd6548998e92660dda01b012983ea
田旗舰 <19373135@buaa.edu.cn>
2021-10-10 01:15:58 +0800

    “url冲突”

--- spoc/backend/urls.py
+++ spoc/backend/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('StudentRegister/', views.StudentRegister.as_view()),
     path('TeacherLogin/', views.TeacherLogin.as_view()),
     path('GetCourseList/', views.GetCourseList.as_view()),
+    path('GetMyCourseList/', views.GetMyCourseList.as_view()),
+    path('DropCourse/', views.DropCourse.as_view()),
     path('SelectCourse/', views.SelectCourse.as_view()),
     path('StudentChange/', views.StudentChange.as_view()),
     path('TeacherChange/', views.TeacherChange.as_view())
