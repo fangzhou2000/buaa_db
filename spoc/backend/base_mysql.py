fcd37c6f1d302e6f9883c66ac9b57fb683fc6d23
Mango <2732834341@qq.com>
2021-10-11 01:46:47 +0800

    Update: 增加修改密码，增加课程的SQL操作

--- spoc/backend/base_mysql.py
+++ spoc/backend/base_mysql.py
@@ -1,159 +1,188 @@
 import pymysql
 
 
-def connect_database():
-    connection = pymysql.connect(host="rm-2zeu3f7e1n5yt10v0co.mysql.rds.aliyuncs.com",
-                                 db="spoc",
-                                 user="root",
-                                 passwd="myja&*$4X579cKr",
-                                 charset="utf8")
-    cursor = connection.cursor()
-    return connection, cursor
+class baseSQL:
 
+    def connect_database(self):
+        connection = pymysql.connect(host="rm-2zeu3f7e1n5yt10v0co.mysql.rds.aliyuncs.com",
+                                     db="spoc",
+                                     user="root",
+                                     passwd="myja&*$4X579cKr",
+                                     charset="utf8")
+        cursor = connection.cursor()
+        return connection, cursor
 
-def find_student_course(userName, id):
-    connection, cursor = connect_database()
+    def find_student_course(self, userName, id):
+        connection, cursor = self.connect_database()
 
-    instruction = "SELECT *\
-                FROM stu_course_list\
-                Where username=%s AND id=%s"
+        instruction = "SELECT *\
+                    FROM stu_course_list\
+                    Where username=%s AND id=%s"
 
-    cursor.execute(instruction, [userName, id])
+        cursor.execute(instruction, [userName, id])
 
-    result = cursor.fetchall()
+        result = cursor.fetchall()
 
-    close_database(connection, cursor)
-    return result
+        self.close_database(connection, cursor)
+        return result
 
+    def select_course(self, userName, id):
+        connection, cursor = self.connect_database()
 
-def select_course(userName, id):
-    connection, cursor = connect_database()
+        instruction = "INSERT INTO stu_course_list " \
+                      "values(%s, %s)"
 
-    instruction = "INSERT INTO stu_course_list " \
-                  "values(%s, %s)"
+        cursor.execute(instruction, [userName, id])
+        connection.commit()
 
-    cursor.execute(instruction, [userName, id])
-    connection.commit()
+        self.close_database(connection, cursor)
 
-    close_database(connection, cursor)
+        return
 
-    return
+    def drop_student_course(self, userName, id):
+        connection, cursor = self.connect_database()
 
+        instruction = "DELETE FROM stu_course_list " \
+                      "WHERE username=%s AND id=%s "
+        cursor.execute(instruction, [userName, id])
+        connection.commit()
 
-def drop_student_course(userName, id):
-    connection, cursor = connect_database()
+        self.close_database(connection, cursor)
+        return
 
-    instruction = "DELETE FROM stu_course_list " \
-                  "WHERE username=%s AND id=%s "
-    cursor.execute(instruction, [userName, id])
-    connection.commit()
+    def get_student_course(self, userName):
+        connection, cursor = self.connect_database()
 
-    close_database(connection, cursor)
-    return
+        instruction = "SELECT course.id, name " \
+                      "FROM course, stu_course_list " \
+                      "WHERE course.id=stu_course_list.id AND username=%s " \
+                      "ORDER BY course.id"
 
+        cursor.execute(instruction, [userName])
 
-def get_student_course(userName):
-    connection, cursor = connect_database()
+        result = cursor.fetchall()
+        self.close_database(connection, cursor)
 
-    instruction = "SELECT course.id, name " \
-                  "FROM course, stu_course_list " \
-                  "WHERE course.id=stu_course_list.id AND username=%s " \
-                  "ORDER BY course.id"
+        return result
 
-    cursor.execute(instruction, [userName])
+    def get_all_course(self):
+        connection, cursor = self.connect_database()
 
-    result = cursor.fetchall()
-    close_database(connection, cursor)
+        instruction = "SELECT * " \
+                      "FROM course"
 
-    return result
+        cursor.execute(instruction)
+        result = cursor.fetchall()
+        self.close_database(connection, cursor)
+        return result
 
+    def create_course(self, userName, courseName):
+        connection, cursor = self.connect_database()
 
-def get_all_course():
-    connection, cursor = connect_database()
+        instruction = "INSERT INTO course(name) " \
+                      "VALUES(%s)"
+        cursor.execute(instruction, [courseName])
 
-    instruction = "SELECT * " \
-                  "FROM course"
+        connection.commit()
 
-    cursor.execute(instruction)
-    result = cursor.fetchall()
-    close_database(connection, cursor)
-    return result
+        self.close_database(connection, cursor)
 
+    def close_database(self, connection, cursor):
+        connection.close()
+        cursor.close()
 
-def close_database(connection, cursor):
-    connection.close()
-    cursor.close()
+    def create_student(self, userName, passWord):
+        connection, cursor = self.connect_database()
 
+        instruction = "INSERT INTO student " \
+                      "values(%s, %s)"
 
-def create_student(userName, passWord):
-    connection, cursor = connect_database()
+        cursor.execute(instruction, [userName, passWord])
+        connection.commit()
 
-    instruction = "INSERT INTO student " \
-                  "values(%s, %s)"
+        self.close_database(connection, cursor)
+        return
 
-    cursor.execute(instruction, [userName, passWord])
-    connection.commit()
+    def create_teacher(self, userName, passWord):
+        connection, cursor = self.connect_database()
 
-    close_database(connection, cursor)
-    return
+        instruction = "INSERT INTO teacher " \
+                      "values(%s, %s)"
 
+        cursor.execute(instruction, [userName, passWord])
+        connection.commit()
 
-def create_teacher(userName, passWord):
-    connection, cursor = connect_database()
+        self.close_database(connection, cursor)
+        return
 
-    instruction = "INSERT INTO teacher " \
-                  "values(%s, %s)"
+    def find_teacher(self, userName):
+        connection, cursor = self.connect_database()
 
-    cursor.execute(instruction, [userName, passWord])
-    connection.commit()
+        instruction = "SELECT *\
+                FROM teacher\
+                Where username=%s"
 
-    close_database(connection, cursor)
-    return
+        cursor.execute(instruction, [userName])
 
+        result = cursor.fetchall()
 
-def find_teacher(userName):
-    connection, cursor = connect_database()
+        self.close_database(connection, cursor)
 
-    instruction = "SELECT *\
-            FROM teacher\
-            Where username=%s"
+        return result
 
-    cursor.execute(instruction, [userName])
+    def find_student(self, userName):
+        connection, cursor = self.connect_database()
 
-    result = cursor.fetchall()
+        instruction = "SELECT *\
+                FROM student\
+                Where username=%s"
 
-    close_database(connection, cursor)
+        cursor.execute(instruction, [userName])
 
-    return result
+        result = cursor.fetchall()
 
+        self.close_database(connection, cursor)
 
-def find_student(userName):
-    connection, cursor = connect_database()
+        return result
 
-    instruction = "SELECT *\
-            FROM student\
-            Where username=%s"
+    def change_stu_password(self, userName, passWord):
+        connection, cursor = self.connect_database()
 
-    cursor.execute(instruction, [userName])
+        instruction = "UPDATE student " \
+                      "SET password=%s " \
+                      "WHERE username=%s"
+        cursor.execute(instruction, [passWord, userName])
 
-    result = cursor.fetchall()
+        connection.commit()
+        self.close_database(connection, cursor)
+        return
 
-    close_database(connection, cursor)
+    def change_teacher_password(self, userName, passWord):
+        connection, cursor = self.connect_database()
 
-    return result
+        instruction = "UPDATE teacher " \
+                      "SET password=%s " \
+                      "WHERE username=%s"
+        cursor.execute(instruction, [passWord, userName])
+
+        connection.commit()
+        self.close_database(connection, cursor)
+        return
 
 
 if __name__ == "__main__":
     userName = "19373136"
-
-    drop_student_course(userName, "5")
+    sql = baseSQL()
+    sql.drop_student_course(userName, "5")
     # select_course(userName, "2")
-    result = get_student_course(userName)
+    result = sql.get_student_course(userName)
     print(result)
 
-    result = find_student_course(userName, "4")
+    result = sql.find_student_course(userName, "4")
     flag = not not result
     print(flag)
+    sql.change_teacher_password("123", "123456")
+    # sql.create_course("13","线性代数2")
     """
     create table
     create_student = "CREATE TABLE `student` (`username` varchar(20) NOT NULL PRIMARY KEY, " \
--- spoc/backend/views.py
+++ spoc/backend/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .base_mysql import *
+from .base_mysql import baseSQL
 
 
 class StudentLogin(APIView):
@@ -10,8 +10,8 @@ class StudentLogin(APIView):
         userPassWord = str(request.GET.get('userPassWord', None))
         print("StudentLogin得到的学号和密码是 " + userName + " " + userPassWord)
         # 下面仅仅是模拟测试，需要从学生用户的表检索，不存在学号或学号为空返回1，密码错误返回2，登录成功返回0
-        # todo
-        result = find_student(userName)
+        sql = baseSQL()
+        result = sql.find_student(userName)
         flag = not not result
 
         if flag:
@@ -35,9 +35,8 @@ class StudentRegister(APIView):
         userPassWord2 = str(request.GET.get('userPassWord2', None))
         print("StudentRegister得到的学号和密码和确认密码是 " + userName + " " + userPassWord + " " + userPassWord2)
         # 下面仅仅是模拟测试，需要从学生用户的表中查询和写入，已存在学号返回1，密码不一致返回2，登成功返回0
-        # todo
-
-        result = find_student(userName)
+        sql = baseSQL()
+        result = sql.find_student(userName)
         flag = not not result
 
         if flag:
@@ -50,7 +49,7 @@ class StudentRegister(APIView):
         elif userPassWord != userPassWord2:
             return Response(2)
         else:
-            create_student(userName, userPassWord)
+            sql.create_student(userName, userPassWord)
             return Response(0)
 
 
@@ -60,9 +59,9 @@ class TeacherLogin(APIView):
         userPassWord = str(request.GET.get('userPassWord', None))
         print("TeacherLogin得到的学号和密码是 " + userName + " " + userPassWord)
         # 下面仅仅是模拟测试，需要从教师用户的表检索，不存在工号或工号为空返回1，密码错误返回2，登录成功返回0
-        # todo
 
-        result = find_teacher(userName)
+        sql = baseSQL()
+        result = sql.find_teacher(userName)
         flag = not not result
 
         if flag:
@@ -94,7 +93,9 @@ class TeacherRegister(APIView):
         print("TacherRegister得到的工号和密码和确认密码是 " + userName + " " + userPassWord + " " + userPassWord2)
         # 下面仅仅是模拟测试，需要从教师用户的表中查询和写入，已存在工号返回1，密码不一致返回2，登录成功返回0
 
-        result = find_teacher(userName)
+        sql = baseSQL()
+
+        result = sql.find_teacher(userName)
         flag = not not result
 
         if flag:
@@ -107,14 +108,15 @@ class TeacherRegister(APIView):
         elif userPassWord != userPassWord2:
             return Response(2)
         else:
-            create_teacher(userName, userPassWord)
+            sql.create_teacher(userName, userPassWord)
             return Response(0)
 
 
 class GetCourseList(APIView):
     def get(self, requset):
         # 从课程表里查询所有课程，返回一个字典的列表，字典中key值为 'id' 和 'name', id为主码
-        result = get_all_course()
+        sql = baseSQL()
+        result = sql.get_all_course()
         print("GetCourseList")
         courses = []  # 字典列表
         for item in result:
@@ -127,15 +129,15 @@ class SelectCourse(APIView):
         # 从课程表里选择课程，先在学生选课表里查询该学生是否已经选了这个课程（通过userName和id），如果已有该课程，直接返回1，如果没有，则在学生选课表中添加该学生和该课程，并返回1
         userName = str(request.GET.get('userName', None))
         id = str(request.GET.get('id', None))
-
-        result = find_student_course(userName, id)
+        sql = baseSQL()
+        result = sql.find_student_course(userName, id)
         flag = not not result
         # flag 表示是否有选课记录，True表示有选课记录。
 
         if flag:
             return Response(1)
         else:
-            select_course(userName, id)
+            sql.select_course(userName, id)
             return Response(0)
         # if 没选:
         #     return Response(0);
@@ -152,9 +154,9 @@ class GetMyCourseList(APIView):
     def get(self, request):
         # 从学生选课表里查询该学生选的课
         userName = str(request.GET.get('userName', None))
-        print('6666666666666666666')
         # 得到学生课程列表ok
-        result = get_student_course(userName)
+        sql = baseSQL()
+        result = sql.get_student_course(userName)
         myCourses = []  # 字典列表
         for item in result:
             myCourses.append(dict([("id", item[0]), ("name", item[1])]))
@@ -168,11 +170,12 @@ class DropCourse(APIView):
         userName = str(request.GET.get('userName', None))
         id = str(request.GET.get('id', None))
 
-        drop_student_course(userName, id)
+        sql = baseSQL()
+        sql.drop_student_course(userName, id)
 
         myCourses = []  # 字典列表
 
-        result = get_student_course(userName)
+        result = sql.get_student_course(userName)
 
         for item in result:
             myCourses.append(dict([("id", item[0]), ("name", item[1])]))
@@ -187,7 +190,16 @@ class StudentChange(APIView):
         userPassWord1 = str(request.GET.get('userPassWord1', None))  # 新密码
         userPassWord2 = str(request.GET.get('userPassWord2', None))  # 确认新密码
         # 从学生用户表里查， 如果原密码不正确返回1，如果正确，如果新密码不一致返回2，如果一致，在学生用户表里修改密码，返回0.
-        return Response(0)
+
+        sql = baseSQL()
+        result = sql.find_student(userName)
+        if userPassWord0 != result[0][1]:
+            return Response(1)
+        elif userPassWord2 != userPassWord1:
+            return Response(2)
+        else:
+            sql.change_stu_password(userName, userPassWord1)
+            return Response(0)
 
 
 class TeacherChange(APIView):
@@ -197,11 +209,23 @@ class TeacherChange(APIView):
         userPassWord1 = str(request.GET.get('userPassWord1', None))  # 新密码
         userPassWord2 = str(request.GET.get('userPassWord2', None))  # 确认新密码
         # 从教师用户表里查， 如果原密码不正确返回1，如果正确，如果新密码不一致返回2，如果一致，在教师用户表里修改密码，返回0.
-        return Response(0)
+        sql = baseSQL()
+        result = sql.find_teacher(userName)
+        if userPassWord0 != result[0][1]:
+            return Response(1)
+        elif userPassWord2 != userPassWord1:
+            return Response(2)
+        else:
+            sql.change_teacher_password(userName, userPassWord1)
+            return Response(0)
 
 
 class BuildCourse(APIView):
     def get(self, request):
         userName = str(request.GET.get('userName', None))
-        courseName = str(request.GET.get('userName', None))
+        courseName = str(request.GET.get('courseName', None))
         # 从课程表里新建课程，这里只提供了课程名称，需要在数据库里分配一个对于该课程唯一的id
+
+        sql = baseSQL()
+
+        sql.create_course(userName, courseName)
