c8a1c4d9b17a522d12d16443dcbb2c1aaf226fe2
Mango <2732834341@qq.com>
2021-11-01 20:56:20 +0800

    revert(setting): 删除无用东西

--- spoc/admin/settings.py
+++ spoc/admin/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
