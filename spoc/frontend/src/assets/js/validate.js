69d3f75becc6b84bc0e8f72e864cf4904823bedc
欧阳奎 <19373686@buaa.edu.cn>
2021-12-11 14:13:45 +0800

    commentAndDiscuss-student finished  --version1

---
+++ spoc/frontend/src/assets/js/validate.js
@@ -0,0 +1,25 @@
+export function validatePassword(rule, value, callback) {
+  const passwordRegex = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/
+  if (value === '' || value === undefined || value === null) {
+    callback()
+  } else {
+    if ((!passwordRegex.test(value)) && value !== '') {
+      callback(new Error('请输入6-16位由数字和字母构成的密码!'))
+    } else {
+      callback()
+    }
+  }
+}
+
+export function validateStudentUserName(rule, value, callback) {
+  const userName = /[0-9]{6}/
+  if (value === '' || value === undefined || value === null) {
+    callback()
+  } else {
+    if ((!userName.test(value)) && value !== '') {
+      callback(new Error('请输入6位由数字构成的学号!'))
+    } else {
+      callback()
+    }
+  }
+}
